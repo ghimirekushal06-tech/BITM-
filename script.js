3921329829a94bf32e38f6d9555193e098cfe587
Kushal Ghimire <ghimirekushal06@gmail.com>
2026-08-12 07:15:06 +0545

    Add files via upload
    deployment image issue fixed

---
+++ script.js
@@ -0,0 +1,17 @@
+var typed = new Typed("#typed-text",{
+    strings:[
+        "Full Stack Developer",
+        "UI/UX Designer",
+        "Video Editor",
+        "WordPress Developer",
+         "Git and Github Uploader"
+        
+    ],
+    typedspeed: 80,
+    backspeed: 50,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|"
+});
